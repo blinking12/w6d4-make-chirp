560f56890487314dd2bf7a57c5c279bd0d7bde57
Troy Denney <tvdenney@gmail.com>
2016-11-03 15:13:38 -0400

    working on js file

--- photo-app/js/global.js
+++ photo-app/js/global.js
@@ -1,3 +1,4 @@
+//Collin's code
 import React from 'react'
 import ReactDOM from 'react-dom'
 
@@ -5,13 +6,35 @@ import PhotoUploader from '../components/PhotoUploader'
 import PhotoDisplayer from '../components/PhotoDisplayer'
 
 window.renderView = function() {
-    ReactDOM.render(
-        <div>
-            <PhotoUploader />
-            <PhotoDisplayer />
-        </div>,
-        document.querySelector('#app')
-    )
+  ReactDOM.render(
+    <div>
+      <PhotoUploader />
+      <PhotoDisplayer />
+    </div>,
+    document.querySelector('#app')
+  )
 }
+
+renderView()
+
+
+
+
+//My code
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+//
+// import PhotoUploader from '../components/PhotoUploader'
+// import PhotoDisplayer from '../components/PhotoDisplayer'
+//
+// window.renderView = function() {
+//     ReactDOM.render(
+//         <div>
+//             <PhotoUploader />
+//             <PhotoDisplayer />
+//         </div>,
+//         document.querySelector('#app')
+//     )
+// }
 
 renderView()
